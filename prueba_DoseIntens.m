7102e91b324fc14023e0239e485e37da1e8a2ea5
Miguel López Aguirre <milop01@ucm.es>
2018-01-19 11:32:53 +0100

    Corregido error en prueba_DoseIntens

--- prueba_DoseIntens.m
+++ prueba_DoseIntens.m
@@ -1,5 +1,5 @@
 function prueba_DoseIntens (ct, pln, Dose, z_cut, TypeDose, Model)
-
+ figure
 % Selector de corte de CT para dosis máxima
 if isempty(z_cut)
     [~,I] = max(Dose(:));
@@ -8,7 +8,7 @@ if isempty(z_cut)
     z_cut = z_dijmax;
 end
 
-
+cd plotting\
 % Gráfica de intesidad de dosis
 axis off
 title(sprintf('%s map for %s model', TypeDose, Model));
@@ -24,5 +24,5 @@ colormap(axesHandle, jet(64));
 caxis([cmin cmax]);
 matRad_plotDoseSlice(axesHandle,Total_dose,3,z_cut,0.01,0.8,jet(64), [cmin cmax+0.05]);
 axis off
-
+cd ..
 end
