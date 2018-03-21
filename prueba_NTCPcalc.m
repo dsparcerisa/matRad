60917767f9bd96e18800ef115697591860ef8a04
Miguel López Aguirre <milop01@ucm.es>
2018-03-21 16:11:06 +0100

    Implementados modelos de NTCP para  H&N

--- prueba_NTCPcalc.m
+++ prueba_NTCPcalc.m
@@ -1,7 +1,9 @@
 %% Funcion para el calculo de los valores de NTCP
 
 % Modelos implementados
-% 1- Fukahori (2016)
+
+% Prostate
+% 1 - Fukahori (2016)
 % 2 - Burman (1991)
 % 3 - Cheung (2004) y (2007)
 % 4 - Liu (2010)
@@ -9,6 +11,14 @@
 % 6 - Peeters (2006)
 % 7 - Shaacke (2016)
 
+%Head and Neck
+% 1 - Semenenko (2008)
+% 2 - Burman (1991)
+% 3 - Eisbruch (1999)
+% 4 - Luxton (2008)
+% 5 - Gay (2007)
+% 6 - Roesink (2004)
+  
 function [NTCP] = prueba_NTCPcalc (pln, cst, phantomtype, Dose)
 
 % Modelo LKB
@@ -139,7 +149,6 @@ if strcmp (phantomtype, 'Prostate')>0
                     m = 0.092;
                     TD_50 = 56.7;
                 end
-                
                 indices     = cst{j,4}{1};
                 EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
                 t =  (EUD - TD_50)/(m * TD_50);
@@ -191,7 +200,7 @@ if strcmp (phantomtype, 'Prostate')>0
     NTCP.Peeters.Model = 'Peeters'' Model';
     NTCP.Peeters.VOI = 'Rectum';
     
-    % bleeding
+    % Bleeding
     NTCP.Peeters.Bleeding.Risk = 'Bleeding';
     numOfVois = size(cst,1);
     for j = 1:numOfVois
@@ -221,7 +230,7 @@ if strcmp (phantomtype, 'Prostate')>0
         end
     end
    
-        % Fecal Incontinence
+    % Fecal Incontinence
     NTCP.Peeters.Fecal_Inc.Risk = 'Fecal Incontinence';
     numOfVois = size(cst,1);
     for j = 1:numOfVois
@@ -261,6 +270,391 @@ if strcmp (phantomtype, 'Prostate')>0
      end
 elseif strcmp (phantomtype, 'Head and Neck')>0
     
+    % Semenenko (2008) -> NTCP xerostomia
+    
+    NTCP.Semenenko.Model = 'Semenenko''s Model';
+    NTCP.Semenenko.Risk = 'Xerostomia';
+    NCTP.Semenenko.VOI = 'Parotid gland';
+    n = 1;
+    m = 0.53;
+    TD_50 = 31.4;
+    for j = 1:numOfVois
+        if strcmpi('Parotid_LT',cst{j,2}) > 0
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Semenenko.Left.NTCP = integral(LKM, -inf, t);
+        elseif strcmpi('Parotid_RT',cst{j,2}) > 0
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Semenenko.Right.NTCP = integral(LKM, -inf, t);
+        end
+        
+    end
+    
+    % Burman (1991) -> NTCP xerostomia
+    
+    NTCP.Burman.Model = 'Burman''s Model';
+    NTCP.Burman.Risk = 'Xerostomia';
+    NTCP.Burman.VOI = 'Parotid gland';
+    n = 0.7;
+    m = 0.18;
+    TD_50 = 46;
+    for j = 1:numOfVois
+        if strcmpi('Parotid_LT',cst{j,2}) > 0
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Burman.Left.NTCP = integral(LKM, -inf, t);
+        elseif strcmpi('Parotid_RT',cst{j,2}) > 0
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Burman.Right.NTCP = integral(LKM, -inf, t);
+        end
+    end
+    
+    % Eisbruch (1999) -> RTOG grade 4 (NTCP Fibrosis)
+    
+    NTCP.Eisbruch.Model = 'Eisbruch''s Model';
+    NTCP.Eisbruch.Risk = 'RTOG grade 4 (fibrosis)';
+    NTCP.Eisbruch.VOI = 'Parotid gland';
+    n = 1;
+    m = 0.18;
+    TD_50 = 28.4;
+    for j = 1:numOfVois
+        if strcmpi('Parotid_LT',cst{j,2}) > 0
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Eisbruch.Left.NTCP = integral(LKM, -inf, t);
+        elseif strcmpi('Parotid_RT',cst{j,2}) > 0
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Eisbruch.Right.NTCP = integral(LKM, -inf, t);
+        end
+    end
+
+    % Luxton (2008) -> Multiple NTCP
+    
+    NTCP.Eisbruch.Model = 'Luxton''s Model';
+    
+    for j = 1:numOfVois
+        if strcmpi('Parotid_LT',cst{j,2}) > 0
+            NTCP.Luxton.Parotid.Risk = 'Xerostomia';
+            NTCP.Luxton.Parotid.VOI = 'Parotid gland';
+            n = 0.7;
+            m = 0.18;
+            TD_50 = 46;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Luxton.Parotid.Left.NTCP = integral(LKM, -inf, t);
+        elseif strcmpi('Parotid_RT',cst{j,2}) > 0
+            NTCP.Luxton.Parotid.Risk = 'Xerostomia';
+            NTCP.Luxton.Parotid.VOI = 'Parotid gland';
+            n = 0.7;
+            m = 0.18;
+            TD_50 = 46;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Luxton.Parotid.Right.NTCP = integral(LKM, -inf, t);
+            
+        elseif strcmpi('Brain_stem',cst{j,2}) > 0
+            NTCP.Luxton.Brain_Stem.Risk = 'Necrosis/Infraction';
+            NTCP.Luxton.Brain_Stem.VOI = 'Brain Stem';
+            n = 0.16;
+            m = 0.15;
+            TD_50 = 65;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Luxton.Brain_Stem.NTCP = integral(LKM, -inf, t);
+            
+        elseif strcmpi('Cerebellum',cst{j,2}) > 0
+            NTCP.Luxton.Cerebellum.Risk = 'Necrosis/Infraction';
+            NTCP.Luxton.Cerebellum.VOI = 'Brain/Cerebellum';
+            n = 0.25;
+            m = 0.15;
+            TD_50 = 60;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Luxton.Cerebellum.NTCP = integral(LKM, -inf, t);
+            
+        elseif strcmpi('Larynx',cst{j,2}) > 0
+            NTCP.Luxton.Larynx.Necro.Risk = 'Cartilague Necrosis';
+            NTCP.Luxton.Larynx.VOI = 'Larynx';
+            n_1 = 0.08;
+            m_1 = 0.17;
+            TD_50_1 = 70;
+            indices     = cst{j,4}{1};
+            EUD_1 = (sum(Dose(indices).^(1/n_1))/numel(indices))^n_1 *(pln.numOfFractions);
+            t_1 =  (EUD_1 - TD_50_1)/(m_1 * TD_50_1);
+            NTCP.Luxton.Larynx.Necro.NTCP = integral(LKM, -inf, t_1);
+            
+            NTCP.Luxton.Larynx.Edema.Risk = 'Laryngeal Edema';
+            n_2 = 0.11;
+            m_2 = 0.075;
+            TD_50_2 = 80;
+            indices     = cst{j,4}{1};
+            EUD_2 = (sum(Dose(indices).^(1/n_2))/numel(indices))^n_2 *(pln.numOfFractions);
+            t_2 =  (EUD_2 - TD_50_2)/(m_2 * TD_50_2);
+            NTCP.Luxton.Larynx.Edema.NTCP = integral(LKM, -inf, t_2);
+            
+        elseif strcmpi('Lens_LT',cst{j,2}) > 0
+            NTCP.Luxton.OcularLens.Risk = 'Cataract Requiring Intervention';
+            NTCP.Luxton.OcularLens.VOI = 'Ocular Lens';
+            n = 0.3;
+            m = 0.27;
+            TD_50 = 18;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Luxton.OcularLens.Left.NTCP = integral(LKM, -inf, t);
+        elseif strcmpi('Lens_RT',cst{j,2}) > 0
+            NTCP.Luxton.Parotid.Risk = 'Cataract Requiring Intervention';
+            NTCP.Luxton.Parotid.VOI = 'Ocular Lens';
+            n = 0.3;
+            m = 0.27;
+            TD_50 = 18;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Luxton.OcularLens.Right.NTCP = integral(LKM, -inf, t);
+            
+        elseif strcmpi('Optic_NRV_LT',cst{j,2}) > 0
+            NTCP.Luxton.OpticNerve.Risk = 'Blindness';
+            NTCP.Luxton.OpticNerve.VOI = 'Optic Nerve';
+            n = 0.25;
+            m = 0.14;
+            TD_50 = 65;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Luxton.OpticNerve.Left.NTCP = integral(LKM, -inf, t);
+        elseif strcmpi('Optic_NRV_RT',cst{j,2}) > 0
+            NTCP.Luxton.OpticNerve.Risk = 'Blindness';
+            NTCP.Luxton.OpticNerve.VOI = 'Optic Nerve';
+            n = 0.25;
+            m = 0.14;
+            TD_50 = 65;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Luxton.OpticNerve.Right.NTCP = integral(LKM, -inf, t);
+            
+        elseif strcmpi('Chiasma',cst{j,2}) > 0
+            NTCP.Luxton.Chiasma.Risk = 'Blindness';
+            NTCP.Luxton.Chiasma.VOI = 'Optic Chiasma';
+            n = 0.25;
+            m = 0.14;
+            TD_50 = 65;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Luxton.Chiasma.NTCP = integral(LKM, -inf, t);
+            
+        elseif strcmpi('Skin',cst{j,2}) > 0
+            NTCP.Luxton.Skin.Risk = 'Necrosis/Ulceration';
+            NTCP.Luxton.Skin.VOI = 'Skin';
+            n = 0.1;
+            m = 0.12;
+            TD_50 = 70;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Luxton.Skin.NTCP = integral(LKM, -inf, t);
+            
+        elseif strcmpi('Spinal_Cord',cst{j,2}) > 0
+            NTCP.Luxton.Spinal_Cord.Risk = 'Myelitis/Necrosis';
+            NTCP.Luxton.Spinal_Cord.VOI = 'Spinal Cord';
+            n = 0.05;
+            m = 0.175;
+            TD_50 = 66.5;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Luxton.Spinal_Cord.NTCP = integral(LKM, -inf, t);
+            
+        elseif strcmpi('TM_JOINT_LT',cst{j,2}) > 0
+            NTCP.Luxton.Tm_Joint.Risk = 'Marked Limit. Joint Funct. ';
+            NTCP.Luxton.Tm_Joint.VOI = 'Temporomandibular Joint';
+            n = 0.07;
+            m = 0.1;
+            TD_50 = 72;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Luxton.Tm_Joint.Left.NTCP = integral(LKM, -inf, t);
+        elseif strcmpi('TM_JOINT_RT',cst{j,2}) > 0
+            NTCP.Luxton.Tm_Joint.Risk = 'Marked Limit. Joint Funct. ';
+            NTCP.Luxton.Tm_Joint.VOI = 'Temporomandibular Joint';
+            n = 0.07;
+            m = 0.1;
+            TD_50 = 72;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+            t =  (EUD - TD_50)/(m * TD_50);
+            NTCP.Luxton.Tm_Joint.Right.NTCP = integral(LKM, -inf, t);   
+            
+        end
+    end
+
+    % Gay (2007) -> Multiple NTCP
+    
+        NTCP.Gay.Model = 'Gay''s Model';
+        
+        if strcmpi('Brain_stem',cst{j,2}) > 0
+            NTCP.Gay.Brain_Stem.Risk = 'Necrosis';
+            NTCP.Gay.Brain_Stem.VOI = 'Brain Stem';
+            a = 7;
+            gamma_50 = 3;
+            TD_50 = 65;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^a)/numel(indices))^(1/a) *(pln.numOfFractions);
+            NTCP.Gay.Brain_Stem.NTCP = 1/(1 + (TD_50/EUD)^(4 * gamma_50));
+            
+        elseif strcmpi('Cerebellum',cst{j,2}) > 0
+            NTCP.Gay.Cerebellum.Risk = 'Necrosis/';
+            NTCP.Gay.Cerebellum.VOI = 'Brain/Cerebellum';
+            a = 5;
+            gamma_50 = 3;
+            TD_50 = 60;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^a)/numel(indices))^(1/a) *(pln.numOfFractions);
+            NTCP.Gay.Cerebellum.NTCP = 1/(1 + (TD_50/EUD)^(4 * gamma_50));
+            
+        elseif strcmpi('Chiasma',cst{j,2}) > 0
+            NTCP.Gay.Chiasma.Risk = 'Blindness';
+            NTCP.Gay.Chiasma.VOI = 'Optic Chiasma';
+            a = 25;
+            gamma_50 = 3;
+            TD_50 = 65;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^a)/numel(indices))^(1/a) *(pln.numOfFractions);
+            NTCP.Gay.Chiasma.NTCP = 1/(1 + (TD_50/EUD)^(4 * gamma_50));
+            
+        elseif strcmpi('Optic_NRV_LT',cst{j,2}) > 0
+            NTCP.Gay.OpticNerve.Risk = 'Blindness';
+            NTCP.Gay.OpticNerve.VOI = 'Optic Nerve';
+            a = 25;
+            gamma_50 = 3;
+            TD_50 = 65;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^a)/numel(indices))^(1/a) *(pln.numOfFractions);
+            NTCP.Gay.OpticNerve.Left.NTCP = 1/(1 + (TD_50/EUD)^(4 * gamma_50));
+        elseif strcmpi('Optic_NRV_RT',cst{j,2}) > 0
+            NTCP.Gay.OpticNerve.Risk = 'Blindness';
+            NTCP.Gay.OpticNerve.VOI = 'Optic Nerve';
+            a = 25;
+            gamma_50 = 3;
+            TD_50 = 65;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^a)/numel(indices))^(1/a) *(pln.numOfFractions);
+            NTCP.Gay.OpticNerve.Right.NTCP = 1/(1 + (TD_50/EUD)^(4 * gamma_50));
+            
+        elseif strcmpi('Lens_LT',cst{j,2}) > 0
+            NTCP.Gay.OcularLens.Risk = 'Cataract Requiring Intervention';
+            NTCP.Gay.OcularLens.VOI = 'Ocular Lens';
+            a = 3;
+            gamma_50 = 1;
+            TD_50 = 18;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^a)/numel(indices))^(1/a) *(pln.numOfFractions);
+            NTCP.Gay.OcularLens.Left.NTCP = 1/(1 + (TD_50/EUD)^(4 * gamma_50));
+        elseif strcmpi('Lens_RT',cst{j,2}) > 0
+            NTCP.Gay.OcularLens.Risk = 'Cataract Requiring Intervention';
+            NTCP.Gay.OcularLens.VOI = 'Ocular Lens';
+            a = 3;
+            gamma_50 = 1;
+            TD_50 = 18;
+            indices     = cst{j,4}{1};
+            EUD = (sum(Dose(indices).^a)/numel(indices))^(1/a) *(pln.numOfFractions);
+            NTCP.Gay.OcularLens.Right.NTCP = 1/(1 + (TD_50/EUD)^(4 * gamma_50));
+               
+        end 
+    
+        % Roesink (2004) -> Salivary Excretion
+    
+        NTCP.Roesink.Model = 'Roesink''s Model';
+        NTCP.Roesink.Risk = 'Xerostomia';
+        NCTP.Roesink.VOI = 'Parotid gland';
+        NTCP.Roesink.Note = 'SEFX = Salivary Excretion Factor < X';
+
+        for j = 1:numOfVois
+            if strcmpi('Parotid_LT',cst{j,2}) > 0
+                n = 1;
+                m = 0.42;
+                TD_50 = 52;
+                indices     = cst{j,4}{1};
+                EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+                t =  (EUD - TD_50)/(m * TD_50);
+                NTCP.Roesink.Left.NTCP_SEF25 = integral(LKM, -inf, t);   
+            elseif strcmpi('Parotid_LT',cst{j,2}) > 0
+                n = 1;
+                m = 0.43;
+                TD_50 = 47;
+                indices     = cst{j,4}{1};
+                EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+                t =  (EUD - TD_50)/(m * TD_50);
+                NTCP.Roesink.Left.NTCP_SEF35 = integral(LKM, -inf, t);
+            elseif strcmpi('Parotid_LT',cst{j,2}) > 0
+                n = 1;
+                m = 0.53;
+                TD_50 = 29;
+                indices     = cst{j,4}{1};
+                EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+                t =  (EUD - TD_50)/(m * TD_50);
+                NTCP.Roesink.Left.NTCP_SEF45 = integral(LKM, -inf, t);
+            elseif strcmpi('Parotid_LT',cst{j,2}) > 0
+                n = 1;
+                m = 0.59;
+                TD_50 = 25;
+                indices     = cst{j,4}{1};
+                EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+                t =  (EUD - TD_50)/(m * TD_50);
+                NTCP.Roesink.Left.NTCP_SEF55 = integral(LKM, -inf, t);
+                
+            elseif strcmpi('Parotid_RT',cst{j,2}) > 0
+                n = 1;
+                m = 0.42;
+                TD_50 = 52;
+                indices     = cst{j,4}{1};
+                EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+                t =  (EUD - TD_50)/(m * TD_50);
+                NTCP.Roesink.Right.NTCP_SEF25 = integral(LKM, -inf, t);   
+            elseif strcmpi('Parotid_RT',cst{j,2}) > 0
+                n = 1;
+                m = 0.43;
+                TD_50 = 47;
+                indices     = cst{j,4}{1};
+                EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+                t =  (EUD - TD_50)/(m * TD_50);
+                NTCP.Roesink.Right.NTCP_SEF35 = integral(LKM, -inf, t);
+            elseif strcmpi('Parotid_RT',cst{j,2}) > 0
+                n = 1;
+                m = 0.53;
+                TD_50 = 29;
+                indices     = cst{j,4}{1};
+                EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+                t =  (EUD - TD_50)/(m * TD_50);
+                NTCP.Roesink.Right.NTCP_SEF45 = integral(LKM, -inf, t);
+            elseif strcmpi('Parotid_RT',cst{j,2}) > 0
+                n = 1;
+                m = 0.59;
+                TD_50 = 25;
+                indices     = cst{j,4}{1};
+                EUD = (sum(Dose(indices).^(1/n))/numel(indices))^n *(pln.numOfFractions);
+                t =  (EUD - TD_50)/(m * TD_50);
+                NTCP.Roesink.Right.NTCP_SEF55 = integral(LKM, -inf, t);
+
+            end
+
+        end
 end
 
 end
